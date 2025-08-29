a8adb5e56bcca187be259a02a8fc5ff8d60ce00b
beratolmez <beratolmez123@hotmail.com>
2025-08-29 15:04:57 +0300

    Signup sayfası ve giriş sayfasında düzenlemeler yapıldı, giriş yönlendirmesi mainPage olarak güncellendi.

--- src/main/java/com/kocaeli/bel/DTO/RegisterHandler.java
+++ src/main/java/com/kocaeli/bel/DTO/RegisterHandler.java
@@ -1,10 +1,20 @@
 package com.kocaeli.bel.DTO;
 import lombok.Getter;
 import lombok.Setter;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 @Getter
 @Setter
 public class RegisterHandler {
     public String TCNo;
-    public String Password;
+    public String isim;
+    
+    @JsonProperty(value = "password", required = true)
+    public String password;
+    
+    // Eski adı için ek destek
+    @JsonProperty("Password")
+    public void setPasswordFromOldField(String password) {
+        this.password = password;
+    }
 }
