c4d88e44726d34df0706874318014ddf0af2b930
Yaşar Can Sandallı <sandalliyasarcan@gmail.com>
2025-08-29 09:18:16 +0300

    Admin Panal/ Haberler sayfası listelemesi yapıldı .

--- src/main/java/com/kocaeli/bel/repository/etkinlik/EtkinlikRepository.java
+++ src/main/java/com/kocaeli/bel/repository/etkinlik/EtkinlikRepository.java
@@ -2,8 +2,12 @@ package com.kocaeli.bel.repository.etkinlik;
 
 import com.kocaeli.bel.model.etkinlik.EtkinlikEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 import java.util.List;
 
+@Repository
 public interface EtkinlikRepository extends JpaRepository<EtkinlikEntity, Long> {
     List<EtkinlikEntity> findAll();
+
 }
