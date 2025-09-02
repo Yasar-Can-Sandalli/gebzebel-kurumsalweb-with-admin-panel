31aad2a2ce851815ac7849086a45e06e39650b2f
Yaşar Can Sandallı <sandalliyasarcan@gmail.com>
2025-09-02 14:55:52 +0300

    Admin Panel / Haberler sayfasına filtreleme, toplu işlemler ve detaylı tablo özelliği eklendi. Müdürlükler için CRUD yapıları ve servisi oluşturuldu.

--- src/main/java/com/kocaeli/bel/model/gebze/TarihceEntity.java
+++ src/main/java/com/kocaeli/bel/model/gebze/TarihceEntity.java
@@ -15,7 +15,8 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class TarihceEntity {
+public class
+TarihceEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
