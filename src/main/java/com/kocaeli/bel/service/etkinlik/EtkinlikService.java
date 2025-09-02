4b89d91a5a99eab926e40038249ea2f5e14c56fc
Yaşar Can Sandallı <sandalliyasarcan@gmail.com>
2025-09-02 14:55:52 +0300

    Admin Panel / Haberler sayfasına filtreleme, toplu işlemler ve detaylı tablo özelliği eklendi. Müdürlükler için CRUD yapıları ve servisi oluşturuldu.

--- src/main/java/com/kocaeli/bel/service/etkinlik/EtkinlikService.java
+++ src/main/java/com/kocaeli/bel/service/etkinlik/EtkinlikService.java
@@ -3,7 +3,6 @@ package com.kocaeli.bel.service.etkinlik;
 import com.kocaeli.bel.model.etkinlik.EtkinlikEntity;
 import com.kocaeli.bel.repository.etkinlik.EtkinlikRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Optional;
