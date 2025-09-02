a32407336602ebaed7ef458a51d4777650ed65a8
Hmzkls12 <hmlzkls12@gmail.com>
2025-09-02 15:17:30 +0300

    Merge from samet

--- MEGAFrontEnd/ReactFront/react/src/components/AdminPanel/sayfalar/gebze/GebzeSanalTurPage.tsx
+++
@@ -1,3 +0,0 @@
-export default function GebzeSanalTurPage() {
-    return <div className="p-2">360 Sanal Tur bağlantıları/medyası burada yönetilecek.</div>;
-}
--- MEGAFrontEnd/ReactFront/react/src/components/AdminPanel/sayfalar/kurumsal/KurumsalVizyonPage.tsx
+++
@@ -1,3 +0,0 @@
-export default function KurumsalVizyonPage() {
-    return <div className="p-2">Vizyon-Misyon-İlke içerikleri burada yönetilecek.</div>;
-}
